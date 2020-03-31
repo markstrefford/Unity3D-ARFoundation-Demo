cc4a5bd71b5b8fb096d16a78e0507e6ba8adb818
Mark Strefford <markstrefford@gmail.com>
2020-03-31 17:23:06 +0100

    Initial code commit

---
+++ Assets/Demo/ARTapToPlaceObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    public GameObject placementIndicator;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator() 
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose() 
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid) 
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+}
